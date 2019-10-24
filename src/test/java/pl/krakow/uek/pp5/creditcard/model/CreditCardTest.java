95bc0a28b8dd3ea4c1afe2ce42823caffdfccf44
qwark97 <marcin.plata1.konta@gmail.com>
2019-10-24 11:09:36 +0200

    Create test to check assign limitations

--- src/test/java/pl/krakow/uek/pp5/creditcard/model/CreditCardTest.java
+++ src/test/java/pl/krakow/uek/pp5/creditcard/model/CreditCardTest.java
@@ -2,12 +2,15 @@ package pl.krakow.uek.pp5.creditcard.model;
 
 import org.junit.Assert;
 import org.junit.Test;
+import pl.krakow.uek.pp5.creditcard.model.exceptions.CreditBelowMinimumException;
 
 import java.math.BigDecimal;
 
 public class CreditCardTest {
 
     public static final int NEW_CREDIT_LIMIT = 2000;
+    public static final int MINIMUM_CREDIT_VALUE_ONE = 100;
+    public static final int MINIMUM_CREDIT_VALUE_TWO = 10000;
 
     @Test
     public void itAllowAssignLimitToCreditCard() {
@@ -17,5 +20,18 @@ public class CreditCardTest {
         card.assignLimit(BigDecimal.valueOf(NEW_CREDIT_LIMIT));
         //Assert //Then //Expect
         Assert.assertTrue(card.getLimit().equals(BigDecimal.valueOf(NEW_CREDIT_LIMIT)));
+    }
+
+    @Test
+    public void itVerifyMinimumCreditValue() {
+        CreditCard card = new CreditCard("1234-5678");
+
+        try {
+            card.assignLimit(BigDecimal.valueOf(50));
+            Assert.fail("Exception should be thrown");
+        } catch (CreditBelowMinimumException e) {
+            Assert.assertTrue(true);
+        }
+
     }
 }
