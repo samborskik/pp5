7e168e4b501d6956264e7344ece01cefd0970895
qwark97 <s-301-21@gumiforest.uek.krakow.pl>
2019-10-24 09:12:12 +0200

    introduce testing class

--- src/main/java/pl/krakow/uek/pp5/creditcard/model/App.java
+++ src/main/java/pl/krakow/uek/pp5/creditcard/model/App.java
@@ -2,6 +2,6 @@ package pl.krakow.uek.pp5.creditcard;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world :)");
     }
 }
