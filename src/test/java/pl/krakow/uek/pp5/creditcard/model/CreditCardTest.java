ad8b95eba1aeea8ecbd15e0bffc5816f70f5bc48
qwark97 <marcin.plata1.konta@gmail.com>
2019-10-24 10:35:50 +0200

    Created test for assignment checking

--- src/test/java/pl/krakow/uek/pp5/creditcard/model/CreditCardTest.java
+++ src/test/java/pl/krakow/uek/pp5/creditcard/model/CreditCardTest.java
@@ -1,4 +1,21 @@
 package pl.krakow.uek.pp5.creditcard.model;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
 public class CreditCardTest {
+
+    public static final int NEW_CREDIT_LIMIT = 2000;
+
+    @Test
+    public void itAllowAssignLimitToCreditCard() {
+        //Arange //Given
+        CreditCard card = new CreditCard("1234-5678");
+        //Act //When
+        card.assignLimit(BigDecimal.valueOf(NEW_CREDIT_LIMIT));
+        //Assert //Then //Expect
+        Assert.assertTrue(card.getLimit().equals(BigDecimal.valueOf(NEW_CREDIT_LIMIT)));
+    }
 }
