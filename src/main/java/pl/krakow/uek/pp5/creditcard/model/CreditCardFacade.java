66082a82e01bdebc03ac7ae4e3cb155970e404fa
s-301-21 <s-301-21@gumiforest.uek.krakow.pl>
2019-11-14 09:00:19 +0100

    Added new tests

--- src/main/java/pl/krakow/uek/pp5/creditcard/model/CreditCardFacade.java
+++ src/main/java/pl/krakow/uek/pp5/creditcard/model/CreditCardFacade.java
@@ -4,7 +4,17 @@ import java.math.BigDecimal;
 
 class CreditCardFacade {
 
-    public void withdrawFromCard(String creditCardNumber, BigDecimal withdrawValue) {
+    private final InMemoryCCStorage storage;
 
+    CreditCardFacade() {
+        this.storage = new InMemoryCCStorage();
+    }
+
+    public void withdrawFromCard(String creditCardNumber, BigDecimal withdrawValue) {
+        CreditCard loaded = storage.load(creditCardNumber);
+
+        loaded.withdraw(withdrawValue);
+
+        storage.add(loaded);
     }
 }
